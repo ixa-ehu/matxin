49006fa809d8d992bd5b14435ff1eb1745f2d4e6
unhammer <unhammer@1a2610ea-1710-4839-aeba-0a8fd53d3058>
2009-11-06 13:27:51 +0000

    initial checking for matxin-fo-en, currently very broken :-)

---
+++ matxin-fo-en.fo.tsx
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tagger name="Faroese">
+  <tagset>
+    <def-label name="NOMF">
+      <tags-item tags="n.f"/>   <!-- probably broken -->
+      <tags-item tags="n.f.*"/>
+    </def-label>
+    <def-label name="NOMM">
+      <tags-item tags="n.m"/>   <!-- probably broken -->
+      <tags-item tags="n.m.*"/>
+    </def-label>
+    <!-- nn nt is ~always ambiguous between sg and pl, uncoarsify todo: -->
+    <def-label name="NOMNT">
+      <tags-item tags="n.nt"/>  <!-- probably broken -->
+      <tags-item tags="n.nt.*"/>
+    </def-label>
+    <def-label name="NOMMF">
+      <tags-item tags="n.mf"/>  <!-- probably broken -->
+      <tags-item tags="n.mf.*"/>
+    </def-label>
+
+    <def-label name="PROPN">
+      <tags-item tags="np.*"/>
+    </def-label>
+
+    <def-label name="NUM">
+      <tags-item tags="num"/>
+      <tags-item tags="num.*"/>
+    </def-label>
+
+    <def-label name="DET">
+      <tags-item tags="det.*"/>
+    </def-label>
+
+    <def-label name="DETDEM">
+      <tags-item tags="det.dem.*"/>
+    </def-label>
+   
+    <def-label name="PRNPERS">
+      <tags-item tags="prn.p1.*"/>
+      <tags-item tags="prn.p2.*"/>
+      <tags-item tags="prn.p3.*"/>
+    </def-label>
+
+    <def-label name="ADJ">
+      <tags-item tags="adj.*"/>
+    </def-label>
+
+    <def-label name="ADV">
+      <tags-item tags="adv"/>
+    </def-label>
+
+    <def-label name="PREP">
+      <tags-item tags="pr"/>
+    </def-label>
+
+    <def-label name="CNJSUB">
+      <tags-item tags="cnjsub"/>
+    </def-label>
+
+    <def-label name="CNJCOO">
+      <tags-item tags="cnjcoo"/>
+    </def-label>
+
+    <def-label name="VBSER">
+      <tags-item tags="vbser.*"/>
+    </def-label>
+
+    <def-label name="VBLEX">
+      <tags-item tags="vblex"/>   <!-- entries like this are probably broken -->
+      <tags-item tags="vblex.*"/>
+    </def-label>
+
+    <def-label name="VBLEXINF">
+      <tags-item tags="vblex.inf"/>
+    </def-label>
+
+  </tagset>
+
+  <forbid>
+    <label-sequence>
+      <label-item label="CNJSUB"/> 
+      <label-item label="CNJSUB"/>
+    </label-sequence>
+  </forbid>
+
+</tagger>
+
