ab81874aea77f14aad8d9cafd722e8c7dfbf4e81
ftyers <ftyers@prompsit.com>
2009-11-13 22:26:19 +0000

    add another test

--- dev/tests/003_xfer-intra_headpos/run-test.sh
+++ dev/tests/003_xfer-intra_headpos/run-test.sh
@@ -4,6 +4,6 @@ INPUT=input.xml
 OUTPUT=output.xml
 EXPECTED=expected_output.xml
 
-cat $INPUT | $BINROOT/matxin-xfer-lex -s $DATAROOT/eu_sem.bin -c $DATAROOT/eseu_chunk_type.txt $DATAROOT/eseu.bin > $OUTPUT
+cat $INPUT | $BINROOT/matxin-xfer-intra $DATADIR/intrachunk_move.dat > $OUTPUT
 
 diff -Naur $EXPECTED $OUTPUT
